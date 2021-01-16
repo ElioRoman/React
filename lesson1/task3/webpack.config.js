b51ad27f0de4dd419c7969328c783e271b21b047
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-16 20:55:09 +0200

    add file

---
+++ lesson1/task3/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    overlay: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
