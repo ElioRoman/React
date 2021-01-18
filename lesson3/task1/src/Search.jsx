9e448a4437950d24a9c28000971f6fbb737ab976
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-18 20:06:40 +0200

    add file

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './search.scss'
 
-export default (props) =>{
-    return (
-      <div className="search">
-        <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-        <div className="search__field">
-          <input type="text" className="search__input" />
-          <button className="search__button">Search</button>
-        </div>
-      </div>
-      )
-  }
+// export default (props) =>{
+//     return (
+//       <div className="search">
+//         <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+//         <div className="search__field">
+//           <input type="text" className="search__input" />
+//           <button className="search__button">Search</button>
+//         </div>
+//       </div>
+//       )
+//   }
+
+export class Search extends Component{
+    render(){
+        return (
+                  <div className="search">
+                    <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+                    <div className="search__field">
+                      <input type="text" className="search__input" />
+                      <button className="search__button">Search</button>
+                    </div>
+                  </div>
+                  )
+     }
+} 
