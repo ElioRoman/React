9d023523cc201fff46e9f37e576825728917b8b8
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-20 22:43:39 +0200

    add file

---
+++ lesson5/HW2/src/ColorPicker.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: "",
+    };
+  }
+
+  pickColor = (text) => {
+    this.setState({
+      color: text,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="picker__title">{this.state.color}</div>
+        <div>
+          <button
+            className="picker__button picker__button_coral"
+            onMouseEnter={() => this.pickColor("Coral")}
+            onMouseLeave={() => this.pickColor("")}
+          ></button>
+          <button
+            className="picker__button picker__button_aqua"
+            onMouseEnter={() => this.pickColor("Aqua")}
+            onMouseLeave={() => this.pickColor("")}
+          ></button>
+          <button
+            className="picker__button picker__button_bisque"
+            onMouseEnter={() => this.pickColor("Bisque")}
+            onMouseLeave={() => this.pickColor("")}
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
