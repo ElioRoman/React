626c451db85c6a1c0b8675b565ed388c541d3cec
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-21 17:07:40 +0200

    add file

---
+++ lesson6/task2/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Mailbox from './Mailbox';
+import "./index.scss";
+
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Mailbox unreadMessages={[]}/>, rootElement);
