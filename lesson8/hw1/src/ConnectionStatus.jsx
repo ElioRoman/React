64e16067673aa18b4a3e986ebbab583ad68af836
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-25 20:38:34 +0200

    add file

---
+++ lesson8/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ConnectionStatus extends Component {
+  state = {
+    status: 'online',
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', this.toggleStatus);
+    window.addEventListener('offline', this.toggleStatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.toggleStatus);
+    window.removeEventListener('offline', this.toggleStatus);
+  }
+
+  toggleStatus = e => {
+    this.setState({
+      status: e.type,
+    });
+  };
+
+  render() {
+
+    const statusChange = this.state.status === 'offline' ? 'status_offline' : '';
+    
+    return(
+    <div className={`status ${statusChange}`}>
+      {this.state.status}
+    </div>);
+  }
+}
+
+export default ConnectionStatus;
