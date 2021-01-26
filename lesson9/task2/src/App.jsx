70131899b9373a881984bb727655251ce7a8ee88
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-26 18:03:29 +0200

    add file

---
+++ lesson9/task2/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  createUser = userObj => {
+    console.log(userObj);
+  };
+  
+  render() {
+    return <UserForm createUser={this.createUser} />;
+  }
+}
+
+export default App;
