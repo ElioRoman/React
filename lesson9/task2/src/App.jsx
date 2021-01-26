18d85264f181089e61e21c1e329ae80b87c67774
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-26 18:15:45 +0200

    add file

--- lesson9/task2/src/App.jsx
+++ lesson9/task2/src/App.jsx
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import UserForm from './UserForm';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   createUser = userObj => {
     console.log(userObj);
   };
-  
+
   render() {
-    return <UserForm createUser={this.createUser} />;
+    return (<><UserForm createUser={this.createUser} /></>)
   }
 }
 
--- lesson9/task2/src/UserForm.jsx
+++ lesson9/task2/src/UserForm.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component {
-  constructor(props){
-    super(props)
-  }
+  
   state = {
     name: '',
     student: '',
