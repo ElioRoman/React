7bd2c03f090b60be83e33bb6354bf35e4f4f78d3
ElioRoman <kolomiets.roman30@gmail.com>
2021-01-27 22:26:33 +0200

    add file

--- lesson10/hw1/src/index.jsx
+++ lesson10/hw1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./index.scss";
-import Calculator from './Calculator'
+import './index.scss';
+import Calculator from './Calculator';
 
 const rootElement = document.querySelector('#root');
 
