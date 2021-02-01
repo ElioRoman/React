3c098a106943284381c5adb3957b78f7bfb2d7df
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-01 13:13:06 +0200

    add file

--- lesson14/hw2/src/App.jsx
+++ lesson14/hw2/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
+
+import Clock from './Clock';
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  const [visible, toggleVisible] = useState(true);
+
+  return (
+    <>
+      <button className="btn" onClick={() => toggleVisible(!visible)}>
+        Toggle
+      </button>
+      {visible && (
+        <>
+          <Clock location={'New York'} offset={-5} />
+          <Clock location={'Kyiv'} offset={2} />
+          <Clock location={'London'} offset={0} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default App;
